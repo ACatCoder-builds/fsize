951e44d736b5b2500a4fc9d29ab6f2a3274a2e2f
ACatCoder <felix508cat@gmail.com>
2025-11-29 18:42:13 +0100

    Clarify supported pointer sizes in lib.rs
    Updated documentation to clarify supported pointer sizes and added compile error for unsupported widths.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! The pointer-sized floating-point type.
 //! 
 //! The size of this primitive is how many bytes it takes to reference any location in memory. For example, on a 32 bit target, this is 4 bytes and on a 64 bit target, this is 8 bytes.
-//! As f8 does not exist 
+//! As f8 does not exist and #[cfg] cannot check for 128 bit systems, only 8, 16 and 32 bits are supported.
 
 #[cfg(target_pointer_width = "16")]
 pub type fsize = f16;
@@ -15,8 +15,5 @@ pub type fsize = f32;
 #[cfg(target_pointer_width = "64")]
 pub type fsize = f64;
 
-#[cfg(target_pointer_width = "128")]
-pub type fsize = f128;
-
-#[cfg(target_pointer_width = "8")]
-compile_error!("8-bit systems are not supported.");
+#[cfg(not(any(target_pointer_width = "16", target_pointer_width = "32", target_pointer_width="64")))]
+compile_error!("Only 16, 32 and 64 bit systems support fsize.");
