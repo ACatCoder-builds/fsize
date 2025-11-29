b00b77365f64cfafb07db87c03bf301c9a0c3a93
ACatCoder <felix508cat@gmail.com>
2025-11-29 18:32:50 +0100

    Add support for f16 and f128 types in fsize

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,10 @@
 //! The pointer-sized floating-point type.
 //! 
 //! The size of this primitive is how many bytes it takes to reference any location in memory. For example, on a 32 bit target, this is 4 bytes and on a 64 bit target, this is 8 bytes.
-//!
-//! As Rust only supports [`f32`](https://doc.rust-lang.org/std/primitive.f32.html) and  [`f64`](https://doc.rust-lang.org/std/primitive.f64.html), `fsize` is only provided for 32 and 64 bit targets.
+//! As f8 does not exist 
+
+#[cfg(target_pointer_width = "16")]
+pub type fsize = f16;
 
 #[cfg(target_pointer_width = "32")]
 pub type fsize = f32;
@@ -13,5 +15,8 @@ pub type fsize = f32;
 #[cfg(target_pointer_width = "64")]
 pub type fsize = f64;
 
-#[cfg(not(any(target_pointer_width = "32", target_pointer_width = "64")))]
-compile_error!("Unsupported target pointer width");
+#[cfg(target_pointer_width = "128")]
+pub type fsize = f128;
+
+#[cfg(target_pointer_width = "8")]
+compile_error!("8-bit systems are not supported.");
